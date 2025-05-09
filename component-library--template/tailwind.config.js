c6c9dab75ed251a579149976c681ac1361fe1ff7
Alex Gichau <gichaumburu@gmail.com>
2025-05-09 16:39:43 +0300

    componet gallery set

--- component-library--template/tailwind.config.js
+++ component-library--template/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
 }
 
